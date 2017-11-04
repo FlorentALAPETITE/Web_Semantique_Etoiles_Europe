9f3f0dd2d39c8725949d5bfecf6dc02d52b453a4
Florent ALAPETITE <florent037@gmail.com>
2017-11-04 17:10:25 +0100

    RDF Construct modification : added structure

--- executeTarql.sh
+++ executeTarql.sh
@@ -1,1 +1,1 @@
-sh target/appassembler/bin/tarql --debug --delimiter=';' ConstructGraph.sparql fr-esr-etoile-de-l-europe.csv
+sh target/appassembler/bin/tarql --debug --delimiter=';' ConstructGraph.sparql fr-esr-etoile-de-l-europe.csv > RDFGraph_Concours_Etoiles_Europes.rdf
