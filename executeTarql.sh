aaaa468eaae74ab676dcf583722c11bf0c629f30
Florent ALAPETITE <florent037@gmail.com>
2017-11-04 16:24:36 +0100

    Construct modification

--- executeTarql.sh
+++ executeTarql.sh
@@ -1,1 +1,1 @@
-sh target/appassembler/bin/tarql --debug --delimiter=';' Test.sparql fr-esr-etoile-de-l-europe.csv
+sh target/appassembler/bin/tarql --debug --delimiter=';' ConstructGraph.sparql fr-esr-etoile-de-l-europe.csv
